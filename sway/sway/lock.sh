c44d2feffb4be940aabb3d338eb9025eabf0e800
Fabian Geiselhart <me@f4814n.de>
2018-05-02 19:33:54 +0200

    Add sway config

---
+++ sway/sway/lock.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+pic="/tmp/pic.png"
+
+# handle being called from systemd service
+# if [ -z "$XDG_RUNTIME_DIR" ] && [ -z "$SWAYSOCK"]; then
+# 	uid=$(id -u $USER)
+# 	export XDG_RUNTIME_DIR="/run/user/"$uid"/"
+# 	export SWAYSOCK=$(find $XDG_RUNTIME_DIR -iname sway*sock)
+# fi
+
+swaygrab $pic
+convert -blur 0x6 $pic $pic
+
+swaylock -i $pic
