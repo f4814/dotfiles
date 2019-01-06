d90a2732fcb4f46a15a8848fac18438d41bec55e
Fabian Geiselhart <me@f4814n.de>
2019-01-06 16:05:24 +0100

    [Sway] Add Lockscreen picture to speedup swaylock

--- sway/.config/sway/lock.sh
+++ sway/.config/sway/lock.sh
@@ -1,28 +1,20 @@
 #!/usr/bin/env bash
 
-pic="/tmp/pic.png"
-
-# handle being called from systemd service
-# if [ -z "$XDG_RUNTIME_DIR" ] && [ -z "$SWAYSOCK"]; then
-# 	uid=$(id -u $USER)
-# 	export XDG_RUNTIME_DIR="/run/user/"$uid"/"
-# 	export SWAYSOCK=$(find $XDG_RUNTIME_DIR -iname sway*sock)
-# fi
+pic=".config/sway/lock.png"
 
 lock() {
-grim -t png $1
-convert -blur 0x6 $1 $1
-swaylock -i $pic --daemonize
+# grim -t png $1
+# convert -blur 0x6 $1 $1
+swaylock -i $1
 }
 
+pkill swaynag # Prevent from double clicking
 swaymsg mode "default"
 
 if [ "$1" == "SUSPEND" ]; then
-    lock $pic
     systemctl suspend
 elif [ "$1" ==  "HIBERNATE" ]; then
-    lock $pic
-    systemctl hibernate -s No
+    systemctl hibernate
 elif [ "$1" == "LOGOUT" ]; then
     swaymsg -r exit
 elif [ "$1" == "REBOOT" ]; then
@@ -33,4 +25,3 @@ else
     lock $pic
 fi
 
-pkill swaynag
